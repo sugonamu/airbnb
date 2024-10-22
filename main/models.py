04f0c5387226aa6dad9df1dc24f8f4ccddc07a00
sugonamu <je0jwff@gmail.com>
2024-10-22 19:51:39 +0700

    hotel management module

--- main/models.py
+++ main/models.py
@@ -17,3 +17,20 @@ class Property(models.Model):
 
     def __str__(self):
         return self.title
+
+class Booking(models.Model):
+    guest = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bookings')
+    property = models.ForeignKey(Property, on_delete=models.CASCADE, related_name='bookings')
+    check_in = models.DateField()
+    check_out = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Booking for {self.property.title} by {self.guest.username}"
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=[('guest', 'Guest'), ('host', 'Host')], default='guest')
+
+    def __str__(self):
+        return self.user.username
--- main/views.py
+++ main/views.py
@@ -1,31 +1,50 @@
 import datetime
-from django.shortcuts import render, redirect, get_object_or_404, reverse
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate, login
-from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth import authenticate, login, logout as auth_logout
 from django.contrib import messages
-from django.http import HttpResponse,HttpResponseRedirect, JsonResponse, HttpResponseForbidden
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse, HttpResponseForbidden
 from django.urls import reverse
-from .forms import  PropertyForm
-from .models import Property, User
+from .forms import PropertyForm
+from .models import Property, Booking, UserProfile, User
+from functools import wraps
 
+def is_host(user):
+    return user.is_authenticated and user.userprofile.role == 'host'
+
+def is_guest(user):
+    return user.is_authenticated and user.userprofile.role == 'guest'
+
+def user_is_host(view_func):
+    @login_required
+    @wraps(view_func)
+    def _wrapped_view(request, *args, **kwargs):
+        if not is_host(request.user):
+            return render(request, 'not_host.html')
+        return view_func(request, *args, **kwargs)
+    return _wrapped_view
+
+@user_is_host
+def host_dashboard(request):
+    properties = request.user.properties.all()
+    bookings = Booking.objects.filter(property__in=properties)
+    return render(request, 'host_dashboard.html', {'properties': properties, 'bookings': bookings})
 
 def home(request):
     if not request.user.is_authenticated:
         return redirect('main:login')
     properties = Property.objects.order_by('?')[:6]
-    user_properties = request.user.properties.all()  
+    user_properties = request.user.properties.all() if request.user.userprofile.role == 'host' else []
     return render(request, 'home.html', {
         'properties': properties,
         'user_properties': user_properties,
         'last_login': request.COOKIES.get('last_login')
     })
 
-
 def login_view(request):
     if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST) 
+        form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
@@ -33,15 +52,13 @@ def login_view(request):
             if user is not None:
                 login(request, user)
                 response = redirect('main:home')
-                response.set_cookie('last_login', str(datetime.datetime.now())) 
+                response.set_cookie('last_login', str(datetime.datetime.now()))
                 return response
             else:
                 messages.error(request, "Invalid username or password.")
     else:
-        form = AuthenticationForm() 
-
+        form = AuthenticationForm()
     return render(request, 'login.html', {'form': form})
-
 
 def register(request):
     if request.method == 'POST':
@@ -50,6 +67,10 @@ def register(request):
             user = form.save(commit=False)
             user.set_password(form.cleaned_data['password1'])
             user.save()
+
+            role = request.POST.get('role')
+            UserProfile.objects.create(user=user, role=role)
+
             messages.success(request, "Registration successful. You can now log in.")
             return redirect('main:login')
     else:
@@ -59,58 +80,63 @@ def register(request):
 def logout(request):
     auth_logout(request)
     response = HttpResponseRedirect(reverse('main:login'))
-    response.delete_cookie('last_login') 
+    response.delete_cookie('last_login')
     return response
-
-def error(request):
-    return render(request, 'error.html') 
 
 def property_detail(request, property_id):
     property_instance = get_object_or_404(Property, id=property_id)
     return render(request, 'property_detail.html', {'property': property_instance})
 
+@user_is_host
 def add_property(request):
     if request.method == "POST":
         form = PropertyForm(request.POST, request.FILES)
         if form.is_valid():
-            property_instance = form.save(commit=False)  
-            property_instance.host = request.user  
-            property_instance.save() 
-            return redirect('main:home')  
+            property_instance = form.save(commit=False)
+            property_instance.host = request.user
+            property_instance.save()
+            return redirect('main:home')
     else:
         form = PropertyForm()
     return render(request, 'addproperty.html', {'form': form})
-    
-@login_required
+
+@user_is_host
 def edit_property(request, property_id):
-    property_instance = get_object_or_404(Property, id=property_id, host=request.user) 
+    property_instance = get_object_or_404(Property, id=property_id, host=request.user)
 
     if request.method == "POST":
         form = PropertyForm(request.POST, request.FILES, instance=property_instance)
         if form.is_valid():
             form.save()
             messages.success(request, "Property updated successfully.")
-            return redirect('main:my_properties')  
+            return redirect('main:my_properties')
     else:
         form = PropertyForm(instance=property_instance)
 
     return render(request, 'editproperty.html', {'form': form})
 
-
-def delete_property(request, product_id):
-    property_instance = get_object_or_404(Property, id=product_id)  # Use id for UUID
+@user_is_host
+def delete_property(request, property_id):
+    property_instance = get_object_or_404(Property, id=property_id)
     property_instance.delete()
     return HttpResponseRedirect(reverse('main:home'))
 
 def property_list(request):
-    properties = request.user.properties.all() 
-    return render(request, 'property_list.html', {'properties': properties})
+    properties = Property.objects.all()
+    user_properties = request.user.properties.all() if request.user.is_authenticated else []
+    
+    return render(request, 'property_list.html', {
+        'properties': properties,
+        'user_properties': user_properties
+    })
 
 @login_required
 def user_list(request):
-    # Replace 'your_username' with your actual username
     if request.user.username != 'fact':
         return HttpResponseForbidden("You are not allowed to view this page.")
     
     users = User.objects.all()
     return render(request, 'user_list.html', {'users': users})
+
+def error(request):
+    return render(request, 'error.html')
